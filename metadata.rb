b421b975e22f6e9c194255d87f4e693aae4dd5f6
Justin Aiken <60tonangel@gmail.com>
2015-08-13 11:26:22 -0600

    Version bump: 0.0.4

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email '60tonangel@gmail.com'
 license          'MIT'
 description      "Installs+configures openhab"
 long_description 'Sets up openhab'
-version          '0.0.3'
+version          '0.0.4'
 
 supports 'debian'
 supports 'raspbian'
